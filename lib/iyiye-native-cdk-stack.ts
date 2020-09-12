d5fa7b0d6899d797d0b64244965fc167d5fbca19
100117 <vahdetkeskin@gmail.com>
2020-09-12 19:14:45 +0300

    add cognito user pool

--- lib/iyiye-native-cdk-stack.ts
+++ lib/iyiye-native-cdk-stack.ts
@@ -1,9 +1,18 @@
-import * as cdk from '@aws-cdk/core';
+import { Mfa, UserPool } from '@aws-cdk/aws-cognito'
+import * as cdk from '@aws-cdk/core'
+import * as config from '../config'
 
 export class IyiyeNativeCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
     // The code that defines your stack goes here
+    new UserPool(this, 'IyiyeUserPool', {
+      userPoolName: config.cognitoUserPoolName,
+      mfa: Mfa.OFF,
+      autoVerify: {
+        email: true
+      }
+    })
   }
 }
