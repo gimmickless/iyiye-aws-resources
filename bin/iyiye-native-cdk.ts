695c2cc1e1f8638ac8bc0287d0eb1aea0823c32b
100117 <vahdetkeskin@gmail.com>
2020-09-13 22:32:44 +0300

    lint & type support

--- bin/iyiye-native-cdk.ts
+++ bin/iyiye-native-cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { IyiyeNativeCdkStack } from '../lib/iyiye-native-cdk-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { IyiyeNativeCdkStack } from '../lib/iyiye-native-cdk-stack'
 
-const app = new cdk.App();
-new IyiyeNativeCdkStack(app, 'IyiyeNativeCdkStack');
+const app = new cdk.App()
+new IyiyeNativeCdkStack(app, 'IyiyeNativeCdkStack')
