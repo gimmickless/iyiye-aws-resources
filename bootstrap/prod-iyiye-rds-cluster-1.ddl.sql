d3eac29acb460ab46d29d1af24b6ccf5ee4fa9bf
vahdet <vahdetkeskin@gmail.com>
2021-04-08 01:08:21 +0300

    more on db scripts

--- bootstrap/prod-iyiye-rds-cluster-1.ddl.sql
+++ bootstrap/prod-iyiye-rds-cluster-1.ddl.sql
@@ -1,6 +1,6 @@
 /* Notification DB (a.k.a notif) */
 
-CREATE DATABASE IF NOT EXISTS notif;
+CREATE DATABASE IF NOT EXISTS notif; --Notification
 CREATE TABLE IF NOT EXISTS notif.in_app_notifications (
     `id` int AUTO_INCREMENT PRIMARY KEY,
     `type` ENUM('announcement', 'promotion', 'report', 'comment', 'star', 'flag'),
@@ -11,3 +11,8 @@ CREATE TABLE IF NOT EXISTS notif.in_app_notifications (
     `last_updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     INDEX `receiver_username_index` (receiver_username)
 );
+
+CREATE DATABASE IF NOT EXISTS order; --Order
+CREATE DATABASE IF NOT EXISTS deliv; --Delivery
+CREATE DATABASE IF NOT EXISTS kit; --Delivery
+
