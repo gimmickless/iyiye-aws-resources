c82afb04944b38d3e2d01d6adb6be7c3b95e183e
vahdet <vahdetkeskin@gmail.com>
2021-05-07 14:45:52 +0300

    format date for ISO8601 on select

--- lib/generic-queries.ts
+++ lib/generic-queries.ts
@@ -7,7 +7,7 @@ Select
   receiver_username As receiverUsername,
   body,
   is_read As isRead,
-  created_time As createdTime,
-  last_updated_time As lastUpdatedTime
+  DATE_FORMAT(created_time, '%Y-%m-%dT%H:%i:%s0Z') As createdTime,
+  DATE_FORMAT(last_updated_time, '%Y-%m-%dT%H:%i:%s0Z') As lastUpdatedTime
   From ${notifDbInAppNotificationsTableName}
 `
