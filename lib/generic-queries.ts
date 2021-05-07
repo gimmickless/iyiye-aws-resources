394e93dcb2c1315fab4e29aaee7b92486d3101f4
vahdet <vahdetkeskin@gmail.com>
2021-05-07 15:05:33 +0300

    fix datetime select formatting

--- lib/generic-queries.ts
+++ lib/generic-queries.ts
@@ -7,7 +7,7 @@ Select
   receiver_username As receiverUsername,
   body,
   is_read As isRead,
-  DATE_FORMAT(created_time, '%Y-%m-%dT%H:%i:%s0Z') As createdTime,
-  DATE_FORMAT(last_updated_time, '%Y-%m-%dT%H:%i:%s0Z') As lastUpdatedTime
+  DATE_FORMAT(created_time, '%Y-%m-%dT%H:%i:%sZ') As createdTime,
+  DATE_FORMAT(last_updated_time, '%Y-%m-%dT%H:%i:%sZ') As lastUpdatedTime
   From ${notifDbInAppNotificationsTableName}
 `
